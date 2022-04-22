a5cf7c55acf1cb43adc7ed344c995d1cbdc22da4
ohmdevpro <99016810+ohmdevpro@users.noreply.github.com>
2022-04-22 20:56:54 +0700

    Add files via upload

---
+++ http.py
@@ -0,0 +1,24 @@
+import requests
+
+username = input("Username ID : ")
+password = input("Password ID : ")
+
+headers = {
+  "Host": "bypass-xattack.000webhostapp.com",
+  "Connection": "keep-alive",
+  "Content-Length": "35",
+  "Accept": "application/json, text/javascript, */*; q=0.01",
+  "X-Requested-With": "XMLHttpRequest",
+  "User-Agent": "Apple",
+  "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+  "Origin": "http://bypass-xattack.000webhostapp.com",
+  "Referer": "http://bypass-xattack.000webhostapp.com/login"
+}
+
+data = {
+  "username":username,
+  "password":password
+}
+
+name = requests.post("http://bypass-xattack.000webhostapp.com/api/login.php",data=data, headers=headers)
+print(name.text)
